2d1f627c3a72f26137493647a0e7f14c6f6b6294
rahimbelaid <rahimbelaid@gmail.com>
2025-07-16 17:45:48 +0000

    Mise à jour du fichier controllers/imageController.js models/Image.js

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,11 +1,12 @@
-const Image = require('../models/Image');
+// imageController.js
+const Carrousel = require('../models/carrousel.model'); // au lieu de Image
 const path = require('path');
 const fs = require('fs');
 
 // Afficher page d’accueil
 exports.afficherAccueil = async (req, res) => {
   try {
-    const imagesCarrousel = await Image.find();
+    const imagesCarrousel = await Carrousel.find(); // Carrousel au lieu de Image
     res.render('index', { imagesCarrousel });
   } catch (error) {
     res.status(500).send('Erreur lors du chargement de la page d\'accueil.');
@@ -15,7 +16,7 @@ exports.afficherAccueil = async (req, res) => {
 // Afficher page admin
 exports.afficherSite = async (req, res) => {
   try {
-    const imagesCarrousel = await Image.find().sort({ createdAt: -1 });
+    const imagesCarrousel = await Carrousel.find().sort({ createdAt: -1 });
     res.render('site', { imagesCarrousel });
   } catch (error) {
     res.status(500).send('Erreur lors du chargement de la page d\'administration.');
@@ -25,7 +26,7 @@ exports.afficherSite = async (req, res) => {
 // Ajouter une image
 exports.ajouterImage = async (req, res) => {
   try {
-    const image = new Image({ url: req.file.filename });
+    const image = new Carrousel({ url: req.file.filename }); // nouveau modèle
     await image.save();
     res.redirect('/admin/site');
   } catch (err) {
@@ -36,22 +37,15 @@ exports.ajouterImage = async (req, res) => {
 // Supprimer une image
 exports.supprimerImage = async (req, res) => {
   try {
-    const image = await Image.findById(req.params.id);
+    const image = await Carrousel.findById(req.params.id);
     if (!image) {
       return res.status(404).send("Image non trouvée.");
     }
-
     const imagePath = path.join(__dirname, '../public/uploads', image.url);
-    console.log("Suppression de :", imagePath);
-
     if (fs.existsSync(imagePath)) {
       fs.unlinkSync(imagePath);
-      console.log("Fichier supprimé du disque.");
-    } else {
-      console.log("Fichier introuvable.");
     }
-
-    await Image.findByIdAndDelete(req.params.id);
+    await Carrousel.findByIdAndDelete(req.params.id);
     res.redirect('/admin/site');
   } catch (err) {
     console.error("Erreur suppression :", err);
